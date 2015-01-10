f0a1c1c9c76cdb15789ac00c49062e438eb09fcf
Nishaanth <nish.h.reddy@gmail.com>
2015-01-10 11:53:06 -0800

    Update PE1.c

--- Comeptitions/Project-Euler/PE1.c
+++ Comeptitions/Project-Euler/PE1.c
@@ -3,17 +3,17 @@
 #include <math.h>
 #include <stdlib.h>
 long long int SumDivisbleBy(long long int n, long long int p){
-return n*(p/n)*((p/n)+1)/2;
+  return n*(p/n)*((p/n)+1)/2;
 }
 int main() {
-long long int T,i,N,c;
-scanf("%lld", &T);
-while(T--)
-{
-scanf("%lld", &N);
-c= SumDivisbleBy(3,N-1)+SumDivisbleBy(5,N-1)-SumDivisbleBy(15,N-1);
-printf("%lld\n",c);
-}
-/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-return 0;
+  long long int T,i,N,c;
+  scanf("%lld", &T);
+  while(T--)
+  {
+    scanf("%lld", &N);
+    c= SumDivisbleBy(3,N-1)+SumDivisbleBy(5,N-1)-SumDivisbleBy(15,N-1);
+    printf("%lld\n",c);
+  }
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  return 0;
 }
